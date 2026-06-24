15b36e54120ad35fd3d685739b09be856b5f11d1
vmtienbmu99 <flash16799@gmail.com>
2026-06-24 16:47:27 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -5,5 +5,5 @@
   API_URL: 'https://script.google.com/a/bmu.edu.vn/macros/s/AKfycbx.../exec'
 */
 window.BMU_CONFIG = {
-  API_URL: 'DAN_LINK_WEB_APP_APPS_SCRIPT_KET_THUC_BANG_EXEC'
+  API_URL: 'https://script.google.com/a/macros/bmu.edu.vn/s/AKfycbxRbkH4z9CwwP_I0h1-CGeBRtnAlzox3HdyOjp-lsx_nf68ojcBsnXr8LwInxPskoU6/exec'
 };
