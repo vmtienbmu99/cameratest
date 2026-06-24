b8a1867ba23a1f40bc3cbe586323ee8ec8ab8a1b
vmtienbmu99 <flash16799@gmail.com>
2026-06-24 16:34:43 +0700

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,9 @@
+/*
+  CẤU HÌNH API BACKEND APPS SCRIPT
+  Sau khi deploy Apps Script Web App, thay API_URL bên dưới bằng link /exec của bạn.
+  Ví dụ:
+  API_URL: 'https://script.google.com/a/bmu.edu.vn/macros/s/AKfycbx.../exec'
+*/
+window.BMU_CONFIG = {
+  API_URL: 'DAN_LINK_WEB_APP_APPS_SCRIPT_KET_THUC_BANG_EXEC'
+};
