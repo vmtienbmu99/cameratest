5e45129ba5d3f52ec3a2721fde0c92a5bb97f4ed
vmtienbmu99 <flash16799@gmail.com>
2026-06-25 14:34:46 +0700

    Delete assets directory

--- assets/config.js
+++
@@ -1,9 +0,0 @@
-/*
-  CẤU HÌNH API BACKEND APPS SCRIPT
-  Sau khi deploy Apps Script Web App, thay API_URL bên dưới bằng link /exec của bạn.
-  Ví dụ:
-  API_URL: 'https://script.google.com/a/bmu.edu.vn/macros/s/AKfycbx.../exec'
-*/
-window.BMU_CONFIG = {
-  API_URL: 'https://script.google.com/a/macros/bmu.edu.vn/s/AKfycbxRbkH4z9CwwP_I0h1-CGeBRtnAlzox3HdyOjp-lsx_nf68ojcBsnXr8LwInxPskoU6/exec'
-};
