dc636d673579ac39337454b433edddaebc30a1d6
vmtienbmu99 <flash16799@gmail.com>
2026-06-25 16:43:46 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.BMU_CONFIG = {
   // Dán Web App URL của Apps Script backend, phải kết thúc bằng /exec.
   // Ví dụ: https://script.google.com/macros/s/AKfycbxxxx/exec
-  API_URL: 'https://script.google.com/a/macros/s/AKfycbxIWCsUGOnDCo_FEQC33siIrtXB01DCx32U1fpc4v47iS5Cx6gw7mflP-nBljLtvA5t/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxIWCsUGOnDCo_FEQC33siIrtXB01DCx32U1fpc4v47iS5Cx6gw7mflP-nBljLtvA5t/exec',
   API_NAME: 'BMU - Đăng ký & Điểm danh'
 
   // Email sinh viên phải thuộc domain này.
